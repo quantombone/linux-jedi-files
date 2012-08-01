fa8debc683a5e173d0c610f16a0ec293746a4ad9
Tomasz Malisiewicz <malist@gmail.com>
2012-08-01 17:59:32 -0300

    Update forward_foxtrot.sh

--- forward_foxtrot.sh
+++ forward_foxtrot.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-#Forword port 5909 from foxtrot (which is running VNC server) to localhost port 
-#5901 so that localhost:1 will be good for VNC
-echo "After typing onega pw, just do xvnc4viewer :1 and use vnc password"
+#Forword port 5910 from foxtrot (which is running VNC server with -localhost flag) to localhost port 
+#5901 over SSH so that localhost:1 will be good for VNC
+#https://github.com/quantombone/linux-jedi-files/blob/master/forward_foxtrot.sh
+
+echo "After typing foxtrot pw, just do xvnc4viewer :1 and use vnc password"
 ssh -L5901:localhost:5910 tomasz@foxtrot.csail.mit.edu
