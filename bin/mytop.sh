0cfca450c43b1bc8054e70800ca83a1a927041fc
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-09-19 15:02:18 -0400

    mytop inside linux files

--- bin/mytop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-T=`ps -eo pcpu | tail -n +2 | awk '{ SUM += $1} END {print int(SUM) }'`
-if [ $T -gt $1 ]
-then
-    echo "Killing proc because top says " $T
-    exit 1
-else
-    exit 0
-fi
