6aa40730e3f0510f5ecfce2bae6df6c0080385a7
Tomasz Malisiewicz <tomasz@csail.mit.edu>
2012-09-19 14:16:36 -0400

    Delete antiquated CMU scripts

--- forward_foxtrot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#Forward port 5910 from foxtrot (which is running VNC server with -localhost flag) to localhost port 
-#5901 over SSH so that localhost:1 will be good for VNC
-#https://github.com/quantombone/linux-jedi-files/blob/master/forward_foxtrot.sh
-
-echo "After typing foxtrot pw, just do xvnc4viewer :1 and use vnc password"
-ssh -L5901:localhost:5910 tomasz@foxtrot.csail.mit.edu
--- forward_onega.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#Forword port 5909 from balaton (which is running VNC server) to localhost port 
-#5901 so that localhost:1 will be good for VNC
-echo "After typing onega pw, just do xvnc4viewer :1 and use vnc password"
-ssh -L5901:localhost:5909 tmalisie@onega
